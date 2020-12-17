d10f608168b9b6021a86c0ccfe353263123b8e5c
Mogreine <north7311@gmail.com>
2020-12-17 12:35:32 +0300

    Solved the problem -- now qr works for matrices (m, n) where m < n.

--- qr_decomposition/qr.py
+++ qr_decomposition/qr.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def qr_householder(A: np.ndarray):
     """
-    Преобразование Хаусхолдер -- линейный оператор вида H = I - 2vv^t, где v -- вектор единичной длины. В частности поз-
+    Преобразование Хаусхолдера -- линейный оператор вида H = I - 2vv^t, где v -- вектор единичной длины. В частности поз
     воляет занулить определенные компоненты вектора. Не сложно заметить (проверяется обычной подстановкой), что
     1. H^t = H
     2. HH^t = I
@@ -13,8 +13,8 @@ def qr_householder(A: np.ndarray):
     под главной диагональю, в итоге получаем произведение ортогональных матриц, которое превращает A в
     верхнетреугольную матрицу R: H @ A = R  ==>  A = H.T @ R.
 
-    Итераций столько же, сколько и столбцов. На каждой итерации i мы составляем матрицу Хаусхолдера так, чтобы знанулить,
-    элементы A[i:, i], кроме первого.
+    Если A.shape = (n, m), то итераций min(n, m). На каждой итерации i мы составляем матрицу Хаусхолдера так,
+    чтобы знанулить, элементы A[i:, i], кроме первого.
 
     Подробнее можно посмотреть здесь (http://mlwiki.org/index.php/Householder_Transformation) и в 5 лекции.
     """
@@ -22,7 +22,7 @@ def qr_householder(A: np.ndarray):
     Q = np.eye(m)  # Orthogonal transform so far
     R = A.copy()  # Transformed matrix so far
 
-    for j in range(n):
+    for j in range(min(n, m)):
         # Find H = I - beta * u * u.T to put zeros below R[j,j]
         x = R[j:, j]
         normx = np.linalg.norm(x)
@@ -46,7 +46,7 @@ def test(A):
     print(f'Q orthogonality: {np.linalg.norm(Q @ Q.T - np.eye(Q.shape[0]), ord=np.inf)}')
 
     lower_tr_max = 0
-    for i in range(R.shape[1] - 1):
+    for i in range(min(R.shape) - 1):
         lower_tr_max = max(lower_tr_max, np.linalg.norm(R[i + 1:, i], ord=np.inf))
     print(f'R lower part norm: {lower_tr_max}')
 
@@ -57,8 +57,8 @@ def rand_tests():
     A = np.random.normal(0, 1, size=(33, 17))
     test(A)
 
-    # A = np.random.normal(2, 33, size=(24, 65))
-    # test(A)
+    A = np.random.normal(2, 33, size=(24, 65))
+    test(A)
 
     A = np.random.normal(4, 77, size=(500, 500))
     test(A)
